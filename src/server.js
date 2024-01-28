052699583f2080453102f33c8eaa15857f5230f3
neerajbachani <neerajbachani2003@gmail.com>
2024-01-28 16:27:01 +0530

    backend setup

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const { connectDb } = require("./config/db")
 const app =require("./index")
 
 
-const PORT = 5454
+const PORT = 4000
 app.listen(PORT, async()=> {
     await connectDb()
     console.log("server started on ", PORT)
