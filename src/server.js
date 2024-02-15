407a25fe521faa5cfffc7592890aa5db9fdfd126
neerajbachani <neerajbachani2003@gmail.com>
2024-02-15 20:30:20 +0530

    vercel 17

--- src/server.js
+++ src/server.js
@@ -1,14 +1,21 @@
-const { connectDb } = require("./config/db")
-const app =require("./index")
-
+const { connectDb } = require("./config/db");
+const express = require("express");
+const app = express();
+const indexRouter = require("./index");
 
 const port = 3000;
 
+app.use(express.json());
+app.use('/api', indexRouter); // Delegate all routes except root to indexRouter
+
+// Handle root endpoint separately
 app.get('/', (req, res) => {
     res.status(200).send({ message: "helo ji", status: true });
 });
-app.listen(port, async()=> {
-    await connectDb()
-    console.log("server started on ", port)
-})
+
+// Start the server
+app.listen(port, async () => {
+    await connectDb();
+    console.log("server started on ", port);
+});
 
