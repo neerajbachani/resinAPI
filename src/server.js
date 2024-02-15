cef36aeb6523ab6bb57869acf7af1f933327c51a
neerajbachani <neerajbachani2003@gmail.com>
2024-02-15 20:28:18 +0530

    vercel 16

--- src/server.js
+++ src/server.js
@@ -3,6 +3,10 @@ const app =require("./index")
 
 
 const port = 3000;
+
+app.get('/', (req, res) => {
+    res.status(200).send({ message: "helo ji", status: true });
+});
 app.listen(port, async()=> {
     await connectDb()
     console.log("server started on ", port)
