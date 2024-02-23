86382b2d4a7a3ddd2f71ae5cb2fa79d333635fc0
neerajbachani <neerajbachani2003@gmail.com>
2024-02-23 15:37:08 +0530

    vercel changes 6

--- src/index.js
+++ src/index.js
@@ -14,34 +14,34 @@ app.get('/',(req,res)=> {
     return res.status(200).send({message: "helo ji", status: true})
 })
 
-const authRouters = require("./routes/authRoute")
-app.use('/auth',authRouters)
+// const authRouters = require("./routes/authRoute")
+// app.use('/auth',authRouters)
 
-const userRouters = require("./routes/userRoute")
-app.use('/api/users',userRouters)
+// const userRouters = require("./routes/userRoute")
+// app.use('/api/users',userRouters)
 
-const productRouter = require("./routes/productRoute")
-app.use('/api/products', productRouter)
+// const productRouter = require("./routes/productRoute")
+// app.use('/api/products', productRouter)
 
-const adminProductRouter = require("./routes/adminProductsRoute")
-app.use('/api/admin/products', adminProductRouter)
+// const adminProductRouter = require("./routes/adminProductsRoute")
+// app.use('/api/admin/products', adminProductRouter)
 
-const cartRouter = require("./routes/cartRoute")
-app.use('/api/cart', cartRouter)
+// const cartRouter = require("./routes/cartRoute")
+// app.use('/api/cart', cartRouter)
 
-const cartItemRouter = require("./routes/cartItemsRoute")
-app.use('/api/cart_items', cartItemRouter)
+// const cartItemRouter = require("./routes/cartItemsRoute")
+// app.use('/api/cart_items', cartItemRouter)
 
-const orderRouter = require("./routes/orderRoute")
-app.use('/api/orders', orderRouter)
+// const orderRouter = require("./routes/orderRoute")
+// app.use('/api/orders', orderRouter)
 
-const adminOrderRouter = require("./routes/adminOrderRoute")
-app.use('/api/admin/orders', adminOrderRouter)
+// const adminOrderRouter = require("./routes/adminOrderRoute")
+// app.use('/api/admin/orders', adminOrderRouter)
 
-const reviewRouter = require("./routes/reviewRoute")
-app.use('/api/reviews', reviewRouter)
+// const reviewRouter = require("./routes/reviewRoute")
+// app.use('/api/reviews', reviewRouter)
 
-const ratingRouter = require("./routes/ratingRoute")
-app.use('/api/ratings', ratingRouter)
+// const ratingRouter = require("./routes/ratingRoute")
+// app.use('/api/ratings', ratingRouter)
 
 module.exports = app;
